db04d05388a9c423d4654a34a4680374b3669d01
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-05-01 14:06:06 -0600

    connection fixed/ and then broke

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,66 +1,59 @@
 const path = require("path")
-// const noteTakerArray = require("../db/db.json")
 const fs = require("fs")
 const uuid = require("uuid")
 
 
 module.exports = function (app) {
 
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"))
-    });
-
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
-    });
-
     app.get("/api/notes", function (req, res) {
-        fs.readFile(__dirname + "./db/db.json", "utf8", (err, notes) => {
+        fs.readFile(path.join(__dirname, "../db/db.json", (err, data) => {
             if (err) throw err;
-            console.log(notes)
-            return res.json(JSON.parse(notes));
-        })
+            console.log(data)
+            return res.json(JSON.parse(data));
+        }));
     });
 
     app.post("/api/notes", function (req, res) {
-        console.log(req.body)
-        let newNotes = req.body;
-        fs.readFile(__dirname + "./db/db.json", "utf8", (err, notes) => {
+        console.log("new note",req.body)
+        fs.readFile(path.join(__dirname,"../db/db.json", (err,data) => {
             if (err) throw err;
+            const newNotes = req.body;
             console.log(newNotes)
-            const notesArray = JSON.parse(notes);
+            const notesArray = JSON.parse(data);
             newNotes.id = uuid.v4();
             notesArray.push(newNotes)
-            fs.writeFile("./db/db.json", JSON.stringify(notesArray), "utf8", (err, notes) => {
+            fs.writeFile(path.join(__dirname,"../db/db.json", JSON.stringify(notesArray), (err,data) => {
                 return res.json(newNotes)
-            })
-        });
-    })
+            }
+            )
+            )
+        }));
+    });
 
-    app.delete("/api/notes/:id", function (req, res) {
-        const noteToDelete = req.params.id;
-        console.log(noteToDelete)
-        fs.readFile(__dirname + "./db/db.json", (err, notes) => {
-            if (err) {
-                console.log(err);
-                res.sendStatus(500);
-                return;
-            }
-            for (let i = 0; i < data.length; i++) {
-                if (notes[i].id === noteToDelete) {
-                    notes.splice(i, 1);
-                    return;
-                }
-            }
-            fs.writeFile(__dirname + "./db/db.json", JSON.stringify(notes), (err) => {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                    return;
-                }
-                res.send("deleted");
-            });
-        });
-    })
+    // app.delete("/api/notes/:id", function (req, res) {
+    //     const noteToDelete = req.params.id;
+    //     console.log(noteToDelete)
+    //     fs.readFile(__dirname + "./db/db.json", (err, notes) => {
+    //         if (err) {
+    //             console.log(err);
+    //             res.sendStatus(500);
+    //             return;
+    //         }
+    //         for (let i = 0; i < data.length; i++) {
+    //             if (notes[i].id === noteToDelete) {
+    //                 notes.splice(i, 1);
+    //                 return;
+    //             }
+    //         }
+    //         fs.writeFile(__dirname + "./db/db.json", JSON.stringify(notes), (err) => {
+    //             if (err) {
+    //                 console.log(err);
+    //                 res.sendStatus(500);
+    //                 return;
+    //             }
+    //             res.send("deleted");
+    //         });
+    //     });
+    // })
 
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
+const fs = require("fs")
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true }))
 app.use(express.static("public"))
 
+require("./Routes/htmlroutes")(app)
 require("./Routes/routes")(app)
 
 // Listen
