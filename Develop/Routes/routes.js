f646ecb06ede056f4d7f24d54e693a3ece5afa97
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-04-30 18:12:27 -0600

    deleting err

--- Develop/Routes/routes.js
+++ Develop/Routes/routes.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const noteTaker = require("../db/db.json")
+const fs = require("fs")
 
 module.exports = function (app){
     app.get('/', function(req, res) {
@@ -17,13 +18,35 @@ module.exports = function (app){
         console.log(req.body)
         req.body.id = noteTaker.length 
         noteTaker.push(req.body);
+        console.log(req.body.id)
         res.json(noteTaker)
 
     });
-    // make a put route
 
     app.delete("/api/notes:id", function(req, res){
-        console.log("req params", req.params.id)
+        const noteToDelete = req.body.id;
+        fs.readFile(__dirname + "/db/db.json", (err, data) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+                return;
+            }
+            for (let i = 0; i < json.length; i++) {
+                if (json[i].id === noteToDelete) {
+                    json.splice(i, 1);
+                    return;
+                }
+            }
+            fs.writeFile(__dirname + "/db/db.json", JSON.stringify(json), (err) => {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                    return;
+                }
+                res.send("deleted");
+            });
+        });
+        // console.log("req params", req.body.id)
         // noteTaker.push(req.body);
         // res.json(noteTaker)
     })
