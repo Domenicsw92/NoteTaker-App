8453e98d601f860a65f6c6e29db2a948ee4af6cf
Domenic Wilhelm <Domenicsw92@gmail.com>
2021-05-02 13:17:40 -0600

    delete error agian

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,13 +1,13 @@
 const path = require("path")
 const fs = require("fs")
 const uuid = require("uuid");
-const { json } = require("body-parser");
+
 
 
 module.exports = function (app) {
 
     app.get("/api/notes", function (req, res) {
-        fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+        fs.readFile(path.join(__dirname, "../db/db.json"),"utf8", (err, data) => {
             if (err) throw err;
             console.log(data)
             return res.json(JSON.parse(data));
@@ -20,11 +20,15 @@ module.exports = function (app) {
         fs.readFile(path.join(__dirname,"../db/db.json"), "utf8", (err,data) => {
             if (err) throw err;
             let newNotes = req.body;
-            console.log(newNotes)
+            console.log("the newest of notes!",newNotes)
             let notesArray = JSON.parse(data);
-            // let id = notesArray[notesArray.length - 1].id +1;
-            // newNotes.id = id
-            // newNotes.id = uuidv4();
+            let id = 1 
+            if (notesArray.length > 0) {
+                id = notesArray[ notesArray.length - 1 ].id + 1;
+            }
+            newNotes.id = id
+            
+            // newNotes.id = uuid.v4();
             console.log(data)
             notesArray.push(newNotes)
             fs.writeFile(path.join(__dirname,"../db/db.json"), JSON.stringify(notesArray), (err,data) => {
@@ -39,20 +43,22 @@ module.exports = function (app) {
     });
 
     app.delete("/api/notes/:id", function (req, res) {
-        const noteToDelete = req.params.id;
+        const noteToDelete = JSON.parse(req.params.id);
         console.log(noteToDelete)
-        fs.readFile(path.join(__dirname , "../db/db.json"), (err, data) => {
+        fs.readFile(path.join(__dirname , "../db/db.json"), "utf8", (err, data) => {
             if (err) {
                 console.log(err);
                 res.sendStatus(500);
                 return;
             }
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].id === noteToDelete) {
-                    data.splice(i, 1);
-                    return;
+            let notesArray = JSON.parse(data)
+            for (let i = 0; i < notesArray.length; i++) {
+                if (notesArray[i].id === noteToDelete) {
+                    console.log(notesArray[i])
+                    notesArray.splice(i, 1);
                 }
             }
+            console.log("-------------->", notesArray)
             fs.writeFile(path.join(__dirname,"../db/db.json"), JSON.stringify(data), (err) => {
                 if (err) {
                     console.log(err);
